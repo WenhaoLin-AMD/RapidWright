3e4839461c6726a2ef45fcd0005393047189181a
Wenhao Lin <wenhao.lin@amd.com>
2024-09-05 08:06:35 -0600

    Update TestNetTools

--- test/src/com/xilinx/rapidwright/rwroute/TestNetTools.java
+++ test/src/com/xilinx/rapidwright/rwroute/TestNetTools.java
@@ -1,41 +1,33 @@
 package com.xilinx.rapidwright.rwroute;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 
 import com.xilinx.rapidwright.design.Design;
 import com.xilinx.rapidwright.design.Net;
 import com.xilinx.rapidwright.design.NetTools;
-import com.xilinx.rapidwright.util.VivadoToolsHelper;
+import com.xilinx.rapidwright.support.RapidWrightDCP;
+
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class TestNetTools {
     @Test
     public void testIsClockNet() {
-        // two global nets in corescore_500 design reported by Vivado
         // command: select_objects [get_nets -filter {Type == "GLOBAL_CLOCK"}]
-        HashSet<String> clkNetNames = new HashSet<>(List.of(
-            "clk_BUFG", 
-            "clock_gen/rst"
+        HashSet<String> clkNetNamesFromVivado = new HashSet<>(List.of(
+            "clk1_IBUF_BUFG",
+            "clk2_IBUF_BUFG"
         ));
 
-        Design design = Design.readCheckpoint("/proj/xcohdstaff7/wenhalin/project/RapidWright/dataset/versal/corescore_500_versal_routed.dcp");
-        List<Net> clkNets = new ArrayList<>();
+        Design design = RapidWrightDCP.loadDCP("two_clk_check_NetTools.dcp");
+        HashSet<String> clkNetNamesFromRWRoute = new HashSet<>();
         for (Net net: design.getNets()) {
             if (NetTools.isClockNet(net)) {
-                clkNets.add(net);
+                clkNetNamesFromRWRoute.add(net.getName());
             }
         }
 
-        for (Net clkNet: clkNets) {
-            if (clkNetNames.contains(clkNet.getName())) {
-                clkNetNames.remove(clkNet.getName());
-            }
-        }
-        Assertions.assertTrue(clkNetNames.isEmpty());
+        Assertions.assertTrue(clkNetNamesFromVivado.equals(clkNetNamesFromRWRoute));
     }
 }
