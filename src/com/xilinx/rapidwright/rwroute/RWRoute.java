95a90c3b4dfd8d0ffa318090b21ffc18981fc989
Wenhao Lin <wenhao.lin@amd.com>
2024-10-18 01:15:03 -0600

    remove temporary ripup in preprocess function

--- src/com/xilinx/rapidwright/rwroute/RWRoute.java
+++ src/com/xilinx/rapidwright/rwroute/RWRoute.java
@@ -223,12 +223,6 @@ public class RWRoute {
             throw new RuntimeException(getUnsupportedSeriesMessage(design.getPart()));
         }
 
-        for (Net net: design.getNets()) {
-            if (!NetTools.isGlobalClock(net) && !net.isStaticNet()) {
-                net.unroute();
-            }
-        }
-
         // Pre-processing of the design regarding physical net names pins
         DesignTools.makePhysNetNamesConsistent(design);
         DesignTools.createPossiblePinsToStaticNets(design);
